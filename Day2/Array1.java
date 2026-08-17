403a3b42adda7967c5057f992bce87287c6541a1
Aaditya979 <aadityatripathi979@gmail.com>
2026-08-17 18:29:46 +0530

    Day2

---
+++ Day2/Array1.java
@@ -0,0 +1,17 @@
+package Day2;
+
+public class Array1 {
+    public static void main(String[] args) {
+
+        int arr[] = {10, 20, 30, 40, 50};
+        int largest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        System.out.println("Largest element is: " + largest);
+    }
+}
